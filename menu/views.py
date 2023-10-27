531525de2372d9d4ab8b87efd855ea817352ff54
FinaDuxy <alinanik-popova@yandex.ru>
2023-10-27 12:45:00 +0300

    добавлена регистрация

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(user.password)  # Hash the password
+            user.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('register')
+    return render(request, 'profile.html', {'user': request.user})
+
+def home(request):
+    return render(request, 'home.html')
